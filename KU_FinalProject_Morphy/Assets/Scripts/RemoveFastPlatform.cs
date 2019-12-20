fa4e92994c181ec44406f1b114343ad5fce4c240
AnujPatwari <33886859+anuj-patwari@users.noreply.github.com>
2019-12-20 01:10:22 +0000

    Level 26 done. Fast platform removal bug fixed.

---
+++ KU_FinalProject_Morphy/Assets/Scripts/RemoveFastPlatform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemoveFastPlatform : MonoBehaviour
+{
+    public Vector2 position;
+    public GameObject platform;
+    public GameObject platformPlacer;
+
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (platform.GetComponent<pCheckFastPlatform>().placed == true)
+        {
+            position = new Vector2(platform.transform.position.x, platform.transform.position.y);
+            GameObject platformToBePlaced = (GameObject)Instantiate(platformPlacer, position, transform.rotation);
+            Destroy(platform);
+            gm.platformIDNumber = 0;
+            gm.fastPlatformCount = gm.fastPlatformCount + 1;
+            gm.fastPlatformCountText.GetComponent<Text>().text = gm.fastPlatformCount.ToString();
+        }
+
+    }
+}
