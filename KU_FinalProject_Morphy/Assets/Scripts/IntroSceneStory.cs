3fa6f460af590b1f9f922dbfbd4463e7d2a6a67d
AnujPatwari <33886859+anuj-patwari@users.noreply.github.com>
2019-12-20 22:44:02 +0000

    Story
    All frames added into the project
    Scenes for Intro and Section 1 completion added and completed

---
+++ KU_FinalProject_Morphy/Assets/Scripts/IntroSceneStory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSceneStory : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "IntroScene")
+        {
+            StartCoroutine(SwitchToLevelOne(15));
+        }
+
+        else if (SceneManager.GetActiveScene().name == "Section1Completed")
+        {
+            StartCoroutine(SwitchToLevelTen(15));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    IEnumerator SwitchToLevelOne(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Level1");
+    }
+
+    IEnumerator SwitchToLevelTen(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("Level10");
+    }
+}
