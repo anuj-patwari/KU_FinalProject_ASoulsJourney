67702e96eac7b0df3e143962e7c71a7d980c4ffa
anuj-patwari <33886859+anuj-patwari@users.noreply.github.com>
2025-01-03 19:46:33 +0530

    Updated the Credits screen

--- KU_FinalProject_Morphy/Assets/Scripts/CreditsManager.cs
+++ KU_FinalProject_Morphy/Assets/Scripts/CreditsManager.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class CreditsManager : MonoBehaviour
 {
 
-    [SerializeField] GameObject leftButton;
-    [SerializeField] GameObject rightButton;
+    //[SerializeField] GameObject leftButton;
+    //[SerializeField] GameObject rightButton;
     [SerializeField] GameObject creditsText;
-    [SerializeField] GameObject specialThanksText;
+    //[SerializeField] GameObject specialThanksText;
 
-    private int menuNumber;
+    //private int menuNumber;
 
     // Start is called before the first frame update
     void Start()
     {
-        menuNumber = 1;
-        leftButton.SetActive(false);
         creditsText.SetActive(true);
-        specialThanksText.SetActive(false);
-        rightButton.SetActive(true);
+        //menuNumber = 1;
+        //specialThanksText.SetActive(false);
+        //leftButton.SetActive(false);
+        //rightButton.SetActive(true);
     }
 
     // Update is called once per frame
@@ -28,14 +28,14 @@ public class CreditsManager : MonoBehaviour
         
     }
 
-    public void SwitchButtonClicked()
+    /*public void SwitchButtonClicked()
     {
         if (menuNumber == 1)
         {
-            rightButton.SetActive(false);
             creditsText.SetActive(false);
             specialThanksText.SetActive(true);
             leftButton.SetActive(true);
+            rightButton.SetActive(false);
             menuNumber = 2;
         }
 
@@ -43,9 +43,33 @@ public class CreditsManager : MonoBehaviour
         {
             leftButton.SetActive(false);
             creditsText.SetActive(true);
-            specialThanksText.SetActive(false);
+            //specialThanksText.SetActive(false);
             rightButton.SetActive(true);
             menuNumber = 1;
         }
+    }*/
+
+    public void FollowMeOnlineButtonClicked()
+    {
+        Application.OpenURL("https://www.anujpatwari.com/links");
+        print("All hail Social Media!");
+    }
+
+    public void GrumpyBilliButtonClicked()
+    {
+        Application.OpenURL("https://www.youtube.com/@GrumpyBilliGaming");
+        print("Billi bhaiya!!");
+    }
+
+    public void OneUpGameDevButtonClicked()
+    {
+        Application.OpenURL("https://www.youtube.com/@OneUpGameDev");
+        print("OneUp boiii!!");
+    }
+
+    public void WebsiteButtonClicked()
+    {
+        Application.OpenURL("https://www.anujpatwari.com");
+        print("Website opened!");
     }
 }
