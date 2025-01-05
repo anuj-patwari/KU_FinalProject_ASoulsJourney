01f66928280a1a2106ef833051723e5b3e41f007
anuj-patwari <33886859+anuj-patwari@users.noreply.github.com>
2025-01-06 02:15:29 +0530

    Death animation bug fix

--- KU_FinalProject_Morphy/Assets/Scripts/Spikes.cs
+++ KU_FinalProject_Morphy/Assets/Scripts/Spikes.cs
@@ -24,11 +24,23 @@ public class Spikes : MonoBehaviour
     {
         if (col.gameObject.name == "Player")
         {
-            col.gameObject.GetComponent<Player>().deathParticles.SetActive(true);
-            col.gameObject.GetComponent<Player>().deathParticles.transform.parent = col.gameObject.GetComponent<Player>().transform.parent;
+            // Get reference to the player
+            Player player = col.gameObject.GetComponent<Player>();
+
+            // Detach and play death particles at the point of death
+            GameObject deathParticles = player.deathParticles;
+            deathParticles.transform.position = col.gameObject.transform.position; // Set to player's current position
+            deathParticles.transform.parent = null; // Detach from player
+            deathParticles.SetActive(true); // Activate the particles
+
+            // Reset player position to respawn point
             col.gameObject.transform.position = gm.startingCoordinates;
+
+            // Reset gravity and other player properties
             col.gameObject.GetComponent<Rigidbody2D>().gravityScale = gm.currentLevelStartingGravity;
             col.gameObject.GetComponent<Player>().runSpeed = 20f;
+
+            // Adjust player rotation based on gravity
             if (gm.currentLevelStartingGravity > 0)
             {
                 col.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
@@ -37,6 +49,8 @@ public class Spikes : MonoBehaviour
             {
                 col.gameObject.transform.eulerAngles = new Vector3(0, 180f, 180f);
             }
+
+            // Call player's Die method
             col.gameObject.GetComponent<Player>().Die();
         }
     }
